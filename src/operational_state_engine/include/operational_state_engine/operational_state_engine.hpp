2327bbada22aba4e748b4c53ee2c3776e503a29e
Aditya Brahmankar <brahmankarnrsa@gmail.com>
2026-08-03 21:33:23 +0000

    refactor: establish manager package structure

--- src/operational_state_engine/include/operational_state_engine/operational_state_engine.hpp
+++ src/operational_state_engine/include/operational_state_engine/operational_state_engine.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
 
-#include <uav_interfaces/msg/vehicle_state.hpp>
 #include <uav_interfaces/msg/vehicle_status.hpp>
+#include <uav_interfaces/msg/vehicle_state.hpp>
 
 class OperationalStateEngine : public rclcpp::Node
 {
@@ -12,13 +11,15 @@ public:
     OperationalStateEngine();
 
 private:
+
     void vehicleStatusCallback(
         const uav_interfaces::msg::VehicleStatus::SharedPtr msg);
 
-    rclcpp::Subscription<uav_interfaces::msg::VehicleStatus>::SharedPtr
-        vehicle_status_sub_;
+    rclcpp::Subscription<
+        uav_interfaces::msg::VehicleStatus>::SharedPtr
+            vehicle_status_sub_;
 
-    rclcpp::Subscription<uav_interfaces::msg::VehicleStatus>::SharedPtr state_sub_;
-
-    rclcpp::Publisher<uav_interfaces::msg::VehicleState>::SharedPtr state_pub_;
+    rclcpp::Publisher<
+        uav_interfaces::msg::VehicleState>::SharedPtr
+            state_pub_;
 };
--- src/operational_state_engine/src/operational_state_engine.cpp
+++ src/operational_state_engine/src/operational_state_engine.cpp
@@ -4,27 +4,20 @@ OperationalStateEngine::OperationalStateEngine()
 : Node("operational_state_engine")
 {
     vehicle_status_sub_ =
-        this->create_subscription<uav_interfaces::msg::VehicleStatus>(
-            "vehicle/status",
-            10,
-            std::bind(
-                &OperationalStateEngine::vehicleStatusCallback,
-                this,
-                std::placeholders::_1));
-
-    state_sub_ =
-        this->create_subscription<uav_interfaces::msg::VehicleStatus>(
-            "vehicle/status",
-            10,
-            std::bind(
-                &OperationalStateEngine::vehicleStatusCallback,
-                this,
-                std::placeholders::_1));
+        this->create_subscription<
+            uav_interfaces::msg::VehicleStatus>(
+                "vehicle/status",
+                10,
+                std::bind(
+                    &OperationalStateEngine::vehicleStatusCallback,
+                    this,
+                    std::placeholders::_1));
 
     state_pub_ =
-        this->create_publisher<uav_interfaces::msg::VehicleState>(
-            "vehicle/state",
-            10);
+        this->create_publisher<
+            uav_interfaces::msg::VehicleState>(
+                "vehicle/state",
+                10);
 }
 
 void OperationalStateEngine::vehicleStatusCallback(
@@ -33,11 +26,17 @@ void OperationalStateEngine::vehicleStatusCallback(
     auto state = uav_interfaces::msg::VehicleState();
 
     if (!msg->connected)
-        state.state = "NO_FC";
+    {
+        state.state = "WAIT_FOR_FC";
+    }
     else if (!msg->gps_fix)
-        state.state = "WAITING_GPS";
+    {
+        state.state = "WAIT_FOR_GPS";
+    }
     else
+    {
         state.state = "READY";
+    }
 
     state_pub_->publish(state);
 }
